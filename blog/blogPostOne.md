8fdd793439c3a950405225cd2e2afbd89b43d3aa
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2019-06-13 15:23:50 +0000

    solved all issues

---
+++ blog/blogPostOne.md
@@ -0,0 +1,19 @@
+---
+title : first blog
+---
+
+
+
+
+**Hello World**
+
+_Good Woo=rk_
+
+```js
+Fhshhshd wsnnuussnuqns
+
+```
+
+*jjjsjs]
+*jsjjsjjsj
+(Good Boy)[www.youtube.com]
---
+++ src/pages/Index.vue
@@ -0,0 +1,59 @@
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>Hello, world!</h1>
+   
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+    <div v-for="post in $page.posts.edges" :key="post.node.id" >
+      <g-link :to="post.node.path">
+        <h1 >{{post.node.title}} </h1>
+
+      </g-link>
+
+    </div>
+
+  </Layout>
+</template>
+
+
+
+<page-query>
+query Posts{
+  posts: allPost{ 
+    edges {
+      node {
+        id
+        title
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
